5b37f57e8b5f8a93c8869013a570d210f86604dc
김태현 <cplkimth@naver.com>
2018-07-17 11:42:09 +0900

    디버깅 연습

--- HelloGit/HelloGit/Program.cs
+++ HelloGit/HelloGit/Program.cs
@@ -9,7 +9,22 @@ namespace HelloGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int a = GetFactorial(1);
+            Console.WriteLine(a);
+
+            int b = GetFactorial(2);
+            Console.WriteLine(b);
+
+            int c = GetFactorial(3);
+            Console.WriteLine(c);
+        }
+
+        static int GetFactorial(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n * GetFactorial(n-1);
         }
     }
 }
